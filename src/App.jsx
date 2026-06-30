c9efcafc9f8aeb7d23d6cae2c9e6edbbf0582603
nixxexe <leonardogoncalves01@outlook.com>
2026-06-30 20:30:47 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import { Container, Row, Image } from 'react-bootstrap'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  
+  return (
+  <>
+    <Container>
+      <Row>
+        <h1>Leonardo</h1>
+
+      </Row>
+
+      <Row>
+        <Image roundedCircle src="images.jpg"/>
+      </Row>
+
+    </Container>
+
+  </>
+
+)}
+export default App
